a28cb91a3b16c6f2cf9e0fce6bd9cfe7c6be05e4
Grizdor <grizdorgames@gmail.com>
2025-06-10 18:27:54 +0200

    Changed dark emerald ore blocks from Block to DropExperienceBlock

--- src/main/java/net/grizdor/pillaged/block/ModBlocks.java
+++ src/main/java/net/grizdor/pillaged/block/ModBlocks.java
@@ -2,14 +2,13 @@ package net.grizdor.pillaged.block;
 
 import net.grizdor.pillaged.Pillaged;
 import net.grizdor.pillaged.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -21,18 +20,13 @@ public class ModBlocks {
             DeferredRegister.createBlocks(Pillaged.MOD_ID);
 
     public static final DeferredBlock<Block> PILLAGER_DEEPSLATE = registerBlock("pillager_deepslate",
-            () -> new Block(BlockBehaviour.Properties.of()
-                    .mapColor(MapColor.DEEPSLATE)
-                    .instrument(NoteBlockInstrument.BASEDRUM)
-                    .requiresCorrectToolForDrops()
-                    .strength(3.5F, 6.0F)
-                    .sound(SoundType.DEEPSLATE_BRICKS)));
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.CHISELED_DEEPSLATE)));
 
-    public static final DeferredBlock<Block> DEEPSLATE_DARK_EMERALD_ORE = registerBlock("deepslate_dark_emerald_ore",
-            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_EMERALD_ORE)));
+    public static final DeferredBlock<DropExperienceBlock> DARK_EMERALD_ORE = registerBlock("dark_emerald_ore",
+            () -> new DropExperienceBlock(UniformInt.of(3, 7), BlockBehaviour.Properties.ofFullCopy(Blocks.EMERALD_ORE)));
 
-    public static final DeferredBlock<Block> DARK_EMERALD_ORE = registerBlock("dark_emerald_ore",
-            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.EMERALD_ORE)));
+    public static final DeferredBlock<DropExperienceBlock> DEEPSLATE_DARK_EMERALD_ORE = registerBlock("deepslate_dark_emerald_ore",
+            () -> new DropExperienceBlock(UniformInt.of(3, 7), BlockBehaviour.Properties.ofFullCopy(Blocks.DEEPSLATE_EMERALD_ORE)));
 
     public static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
